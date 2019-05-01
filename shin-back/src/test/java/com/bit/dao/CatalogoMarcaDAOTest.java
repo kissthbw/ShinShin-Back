3846a76e3db52a70dd20702a62a346a9e5ca5302
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-05-01 14:23:31 -0500

    Add:test de CatalogoMarca

---
+++ shin-back/src/test/java/com/bit/dao/CatalogoMarcaDAOTest.java
@@ -0,0 +1,41 @@
+package com.bit.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bit.config.WebConfig;
+import com.bit.model.CatalogoMarca;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebConfig.class)
+@WebAppConfiguration
+public class CatalogoMarcaDAOTest {
+
+	@Autowired
+	private CatalogoMarcaDAO marcaDAO;
+
+	@Transactional
+	@Test
+	public void crudTest() {
+		CatalogoMarca m = marcaDAO.findByPK(1L);
+		System.out.println(m.getNombreMarca());
+	}
+
+	@Transactional
+	@Test
+	@Rollback(false)
+	public void save() {
+		CatalogoMarca item = new CatalogoMarca();
+		item.setNombreMarca("Chrome");
+		
+		
+		marcaDAO.save(item);
+	}
+	
+}
