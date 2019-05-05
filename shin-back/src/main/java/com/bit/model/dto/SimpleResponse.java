d7c03e95832314faa5a2106f162aab32ce557ee6
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-05-05 15:28:20 -0500

    Add: se agrego variable Id y sus metodos

--- shin-back/src/main/java/com/bit/model/dto/SimpleResponse.java
+++ shin-back/src/main/java/com/bit/model/dto/SimpleResponse.java
@@ -7,7 +7,8 @@ public class SimpleResponse implements Serializable {
 	private static final long serialVersionUID = 3030545634647110162L;
 
 	private String message;
-	private int code;
+	private Integer code;
+	private Long id;
 
 	public String getMessage() {
 		return message;
@@ -17,12 +18,20 @@ public class SimpleResponse implements Serializable {
 		this.message = message;
 	}
 
-	public int getCode() {
+	public Integer getCode() {
 		return code;
 	}
 
-	public void setCode(int code) {
+	public void setCode(Integer code) {
 		this.code = code;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 }
