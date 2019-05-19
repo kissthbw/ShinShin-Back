f8c6a3b4983f89eeae66f220db6766a9e4915497
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-05-19 10:46:26 -0500

    Add: restructuracion de codigo

--- shin-back/src/main/java/com/bit/model/CatalogoTipoProducto.java
+++ shin-back/src/main/java/com/bit/model/CatalogoTipoProducto.java
@@ -7,22 +7,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "catalogo_tipo_producto")
-public class TipoProducto {
+public class CatalogoTipoProducto {
 
 	@Id
-	@Column(name = "id_tipo_producto")
+	@Column(name = "id_catalogo_tipo_producto")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idTipoProducto;
+	private Long idCatalogoTipoProducto;
 
 	@Column(name = "nombre_tipo_producto")
 	private String nombreTipoProducto;
 
-	public void setIdTipoProducto(Long idTipoProducto) {
-		this.idTipoProducto = idTipoProducto;
+	public void setIdCatalogoTipoProducto(Long idCatalogoTipoProducto) {
+		this.idCatalogoTipoProducto = idCatalogoTipoProducto;
 	}
 
-	public Long getIdTipoProducto() {
-		return idTipoProducto;
+	public Long getIdCatalogoTipoProducto() {
+		return idCatalogoTipoProducto;
 	}
 
 	public void setNombreTipoProducto(String nombreTipoProducto) {
