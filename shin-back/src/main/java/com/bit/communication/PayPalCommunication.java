33870e732946f7ab48acec167e4844e17b83aaf1
Juan Osorio <kissthbw@gmail.com>
2019-05-24 10:26:07 -0500

    Update: Se renombra el servicio de Bonificacion a PayPalCommunication

--- shin-back/src/main/java/com/bit/communication/PayPalCommunication.java
+++ shin-back/src/main/java/com/bit/communication/PayPalCommunication.java
@@ -3,6 +3,7 @@ package com.bit.communication;
 import com.bit.exception.CommunicationException;
 import com.bit.model.dto.BonificacionDTO;
 import com.bit.model.dto.SimpleResponse;
+import com.bit.model.dto.request.PayPalPayoutRQT;
 
 /**
  * Se definen las frimas de los metodos que se encargaran de la aplicacion
@@ -10,7 +11,9 @@ import com.bit.model.dto.SimpleResponse;
  * @author juanosorioalvarez
  *
  */
-public interface BonificacionService {
+public interface PayPalCommunication {
+	
+	SimpleResponse token() throws CommunicationException;
 	
 	/**
 	 * A través del API provisto por PayPal, este metodo se encarga de aplicar un abono
@@ -19,7 +22,7 @@ public interface BonificacionService {
 	 * @return SimpleResponse con el estatus del exito o fracaso de la operacion
 	 * @throws CommunicationException en caso de ocurrir un error
 	 */
-	SimpleResponse bonificacionPayPal( BonificacionDTO data ) throws CommunicationException ;
+	SimpleResponse bonificacionPayPal( PayPalPayoutRQT rqt, String bearerAuth ) throws CommunicationException ;
 	
 	/**
 	 * A tarvés del API prvisto por ClickBox, este metodo se encarga de aplicar un abono
