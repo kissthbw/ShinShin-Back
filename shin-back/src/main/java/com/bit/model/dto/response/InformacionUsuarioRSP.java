90d33bb9acc8e76a579b94c3c6a8e534280f4cf8
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-06-05 11:40:07 -0500

    Add: se agrega variable de medios de bonificaccion y sus metodos set y get

--- shin-back/src/main/java/com/bit/model/dto/response/InformacionUsuarioRSP.java
+++ shin-back/src/main/java/com/bit/model/dto/response/InformacionUsuarioRSP.java
@@ -1,6 +1,9 @@
 package com.bit.model.dto.response;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.bit.model.MediosBonificacion;
 
 public class InformacionUsuarioRSP implements Serializable{
 
@@ -8,6 +11,7 @@ public class InformacionUsuarioRSP implements Serializable{
 	
 	private String nombreUsuario;
 	private double bonificacion;
+	private List<MediosBonificacion> mediosBonificacion;
 
 	public String getNombreUsuario() {
 		return nombreUsuario;
@@ -24,4 +28,14 @@ public class InformacionUsuarioRSP implements Serializable{
 	public void setBonificacion(double bonificacion) {
 		this.bonificacion = bonificacion;
 	}
+
+	public List<MediosBonificacion> getMediosBonificacion() {
+		return mediosBonificacion;
+	}
+
+	public void setMediosBonificacion(List<MediosBonificacion> mediosBonificacion) {
+		this.mediosBonificacion = mediosBonificacion;
+	}
+	
+	
 }
