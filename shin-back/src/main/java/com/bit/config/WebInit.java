402f80a1014e19d830541ffd4c9807dd6c0774b4
Juan Osorio <kissthbw@gmail.com>
2019-06-26 12:18:06 -0500

    Add: Se agrega configuracion para subida de archivos

--- shin-back/src/main/java/com/bit/config/WebInit.java
+++ shin-back/src/main/java/com/bit/config/WebInit.java
@@ -1,5 +1,10 @@
 package com.bit.config;
 
+import java.io.File;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -25,4 +30,15 @@ public class WebInit extends AbstractAnnotationConfigDispatcherServletInitialize
 		configurer.enable();
 	}
 	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		String temp = System.getProperty("catalina.base") +  File.separator + "temp";
+		File folder = new File(temp);
+		
+		if( !folder.exists() ){
+			folder.mkdir();
+		}
+		
+		registration.setMultipartConfig(new MultipartConfigElement(temp, 20097152, 40194304, 0));
+	}
 }
