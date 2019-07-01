8b6537c383799e3c58f0c19a029e2f801aa7b35e
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-07-01 11:08:00 -0500

    Add: se enlazan los html

--- shin-back/src/main/java/com/bit/controllers/CatalogosController.java
+++ shin-back/src/main/java/com/bit/controllers/CatalogosController.java
@@ -17,6 +17,7 @@ import com.bit.model.CatalogoTienda;
 import com.bit.model.CatalogoTipoProducto;
 import com.bit.model.Producto;
 import com.bit.service.CatalogoMarcaService;
+import com.bit.service.CatalogoMediosBonificacionService;
 import com.bit.service.CatalogoTiendaService;
 import com.bit.service.CatalogoTipoProductoService;
 import com.bit.service.ProductoService;
@@ -24,9 +25,6 @@ import com.bit.service.ProductoService;
 @Controller
 @RequestMapping("/catalogos")
 public class CatalogosController {
-	
-	@Autowired
-	private ProductoService productoService;
 
 	@Autowired
 	private CatalogoTipoProductoService catalogoTipoProductoService;
@@ -35,7 +33,13 @@ public class CatalogosController {
 	private CatalogoMarcaService catalogoMarcaService;
 
 	@Autowired
+	private CatalogoMediosBonificacionService catalogoMediosBonificacionService;
+
+	@Autowired
 	private CatalogoTiendaService catalogoTiendaService;
+
+	@Autowired
+	private ProductoService productoService;
 
 	@RequestMapping(value = "/departamento/save", method = RequestMethod.GET)
 	public String redireccionaCatDepartamentos(Model model) {
@@ -44,16 +48,17 @@ public class CatalogosController {
 
 		return "cat_departamentos";
 	}
-	
+
 	@RequestMapping(value = "/departamento/save", method = RequestMethod.POST)
 	public String saveMedio(@ModelAttribute CatalogoTipoProducto item, BindingResult errors, Model model) {
 
 		System.out.println(item.getNombreTipoProducto());
-//		catalogoTipoProductoService.registrarCatalogoTipoProductos(item);
+
+		catalogoTipoProductoService.registrarCatalogoTipoProductos(item);
 
 		return "redirect:/catalogos/departamento/save";
 	}
-	
+
 	@RequestMapping(value = "/marca/save", method = RequestMethod.GET)
 	public String redireccionaCatalogoMarca(Model model) {
 
@@ -66,11 +71,12 @@ public class CatalogosController {
 	public String saveMedio(@ModelAttribute CatalogoMarca item, BindingResult errors, Model model) {
 
 		System.out.println(item.getNombreMarca());
-		//catalogoMarcaService.registrarMarcas(item);
 
-		return "redirect:/marca/save";
-	}	
-	
+		catalogoMarcaService.registrarMarcas(item);
+
+		return "redirect:/catalogos/marca/save";
+	}
+
 	@RequestMapping(value = "/medioBonificacion/save", method = RequestMethod.GET)
 	public String redireccionaCatalogoMediosBonificacion(Model model) {
 
@@ -83,14 +89,15 @@ public class CatalogosController {
 	public String saveMedio(@ModelAttribute CatalogoMediosBonificacion item, BindingResult errors, Model model) {
 
 		System.out.println(item.getNombreMedioBonificacion());
-		//catalogoMediosBonificacionService.registrarCatalogoMediosBonificacion(item);
 
-		return "redirect:/medioBonificacion/save";
+		catalogoMediosBonificacionService.registrarCatalogoMediosBonificacion(item);
+
+		return "redirect:/catalogos/medioBonificacion/save";
 	}
-	
+
 	@RequestMapping(value = "tienda/save", method = RequestMethod.GET)
 	public String redirectionCatalogoTienda(Model model) {
-		
+
 		model.addAttribute("item", new CatalogoTienda());
 
 		return "catalogo_tienda";
@@ -100,19 +107,20 @@ public class CatalogosController {
 	public String saveMedio(@ModelAttribute CatalogoTienda item, BindingResult errors, Model model) {
 
 		System.out.println(item.getNombreTienda());
-		//catalogoTiendaService.registrarTiendas(item);
 
-		return "redirect:/tienda/save";
+		catalogoTiendaService.registrarTiendas(item);
+
+		return "redirect:/catalogos/tienda/save";
 	}
-	
+
 	@RequestMapping(value = "/producto/save", method = RequestMethod.GET)
 	public String redireccionaProducto(Model model) {
 
 		model.addAttribute("item", new Producto());
-		
+
 		List<CatalogoMarca> listMarca = new ArrayList<>();
 		model.addAttribute("marcas", catalogoMarcaService.getCatalogoMarca().getMarcas());
-		
+
 		List<CatalogoTipoProducto> listTipo = new ArrayList<>();
 		model.addAttribute("tipos", catalogoTipoProductoService.getCatalogoTipoProductos().getTipoProductos());
 
@@ -127,8 +135,8 @@ public class CatalogosController {
 
 		System.out.println(item.getNombreProducto());
 
-		// productoService.registrarProductos(item);
+		productoService.registrarProductos(item);
 
-		return "redirect:/producto/save";
+		return "redirect:/catalogos/producto/save";
 	}
 }
