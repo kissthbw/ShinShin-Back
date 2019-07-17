b5ccffc0819505f44d6f07d67f0ca6ccd17edddc
Master_Boy <osorio.alvarez.adrian@gmail.com>
2019-07-17 18:18:40 -0500

    Add: se creo login de administador, usuario y empresa y sus controller

---
+++ shin-back/src/main/java/com/bit/controllers/HomeEmpresa.java
@@ -0,0 +1,36 @@
+package com.bit.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bit.model.Usuario;
+import com.bit.service.UsuarioService;
+
+@Controller
+@RequestMapping("/login")
+public class HomeEmpresa {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@RequestMapping(value = "/empresa", method = RequestMethod.GET)
+	public String redireccionaLogin(Model model) {
+		
+		model.addAttribute("item", new Usuario());
+		
+		return "login_empresa";
+	}
+	
+	@RequestMapping(value = "/empresa", method = RequestMethod.POST)
+	public String loginCuenta(@ModelAttribute Usuario item, BindingResult errors, Model model) {
+		
+		usuarioService.findUserByUserAndPassword(item);
+		
+		return "redirect:/catalogos/producto/list";
+	}
+}
