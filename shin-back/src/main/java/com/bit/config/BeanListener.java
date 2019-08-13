68f18cd39759b4545be74430610931e8c3b23247
Juan Osorio <kissthbw@gmail.com>
2019-08-13 16:56:33 -0500

    Add: Se agrega Bean para cargar diccionario de tiendas

---
+++ shin-back/src/main/java/com/bit/config/BeanListener.java
@@ -0,0 +1,36 @@
+package com.bit.config;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bit.common.Utils;
+import com.bit.dao.CatalogoDiccionarioTiendasDAO;
+import com.bit.model.CatalogoDiccionarioTiendas;
+
+@Component
+public class BeanListener implements ApplicationListener<ContextRefreshedEvent> {
+
+	private static final Logger log = LoggerFactory.getLogger(BeanListener.class);
+	
+	@Autowired
+	private CatalogoDiccionarioTiendasDAO diccionario;
+	
+	@Override
+	@Transactional
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		log.info("Cargando diccionario");
+		List<CatalogoDiccionarioTiendas> list = diccionario.getCatalogoDiccionarioTiendas();
+		Utils.cargaCatalogoDocumentosFilenet(list);
+		for(CatalogoDiccionarioTiendas i : list) {
+			log.info("{}: {}", i.getClaveDiccionario(), i.getValorDiccionario());
+		}
+	}
+
+}
